343e1298760911a12774f591d7169b1949890380
Jing Qi <jinqi@redhat.com>
2026-01-16 12:48:51 +0800

    feat(RELEASE-2137): implement a github action for detecting secret leaks
    Signed-off-by: Jing Qi <jinqi@redhat.com>

--- tasks/managed/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
+++ tasks/managed/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
@@ -4,23 +4,23 @@
 kubectl delete secret test-cosign-secret test-cosign-secret-rekor --ignore-not-found
 
 kubectl create secret generic test-cosign-secret\
-  --from-literal=AWS_DEFAULT_REGION=us-test-1\
-  --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
-  --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
-  --from-literal=SIGN_KEY=aws://arn:mykey\
-  --from-literal=REKOR_PUBLIC_KEY=rekor_public_key\
-  --from-literal=PUBLIC_KEY=public_key
+  --from-literal=AWS_DEFAULT_REGION=SENSITIVE_DATA_us-test-1\
+  --from-literal=AWS_ACCESS_KEY_ID=SENSITIVE_DATA_test-access-key\
+  --from-literal=AWS_SECRET_ACCESS_KEY=SENSITIVE_DATA_test-secret-access-key\
+  --from-literal=SIGN_KEY=aws://arn:SENSITIVE_DATA_mykey\
+  --from-literal=REKOR_PUBLIC_KEY=SENSITIVE_DATA_rekor_public_key\
+  --from-literal=PUBLIC_KEY=SENSITIVE_DATA_public_key
 
 kubectl create secret generic test-cosign-secret-rekor\
-  --from-literal=AWS_DEFAULT_REGION=us-test-1\
-  --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
-  --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
-  --from-literal=SIGN_KEY=aws://arn:mykey\
+  --from-literal=AWS_DEFAULT_REGION=SENSITIVE_DATA_us-test-1\
+  --from-literal=AWS_ACCESS_KEY_ID=SENSITIVE_DATA_test-access-key\
+  --from-literal=AWS_SECRET_ACCESS_KEY=SENSITIVE_DATA_test-secret-access-key\
+  --from-literal=SIGN_KEY=aws://arn:SENSITIVE_DATA_mykey\
   --from-literal=REKOR_URL=https://fake-rekor-server\
-  --from-literal=REKOR_PUBLIC_KEY=rekor_public_key\
-  --from-literal=PUBLIC_KEY=public_key
+  --from-literal=REKOR_PUBLIC_KEY=SENSITIVE_DATA_rekor_public_key\
+  --from-literal=PUBLIC_KEY=SENSITIVE_DATA_public_key
 
 # Add mocks to the beginning of task step script
 TASK_PATH="$1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[1].script' "$TASK_PATH"
+yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + "set +x\n" + .spec.steps[1].script' "$TASK_PATH"
